8d006daa70103f2d3acf199cb0cba76fa466cb2e
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2025-02-19 20:12:14 +0900

    테스트 컨트롤러 추가

--- src/test/java/kit/se/capstone2/Capstone2ApplicationTests.java
+++ src/test/java/kit/se/capstone2/Capstone2ApplicationTests.java
@@ -2,8 +2,10 @@ package kit.se.capstone2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // 명시적 테스트 프로필 활성화
 class Capstone2ApplicationTests {
 
 	@Test
