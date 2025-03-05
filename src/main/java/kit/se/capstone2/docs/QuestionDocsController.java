43fdb32046484f8f9d2d9b4314110d0c6e2ba14e
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2025-03-05 19:31:19 +0900

    질문글 관련 API 설계 일부 추가

--- src/main/java/kit/se/capstone2/docs/QuestionDocsController.java
+++ src/main/java/kit/se/capstone2/docs/QuestionDocsController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "QuestionDocsController", description = "질문글 관련 API 문서")
 public interface QuestionDocsController {
-	@Operation(summary = "AI 채팅 세션 생성", description = "채팅 세션을 생성한다.")
+	@Operation(summary = "질문글 목록 조회", description = "질문 글에 대한 목록을 페이지네이션으로 조회한다.")
 	@ApiResponse(responseCode = "200", description = "성공")
 	public CommonResponse<Page<QuestionResponse.Question>> getQuestion(
 			@Schema(description = "페이지 번호", example = "0")
@@ -22,28 +22,28 @@ public interface QuestionDocsController {
 			int size
 	);
 
-	@Operation(summary = "AI 채팅 세션 생성", description = "채팅 세션을 생성한다.")
+	@Operation(summary = "질문글 상세 조회", description = "특정 ID에 대한 질문의 내용을 상세하게 조회한다.")
 	@ApiResponse(responseCode = "200", description = "성공")
 	CommonResponse<QuestionResponse.Question> getQuestionById(
 			@Schema(description = "질문글 ID", example = "1")
 			Long id
 	);
 
-	@Operation(summary = "AI 채팅 세션 생성", description = "채팅 세션을 생성한다.")
+	@Operation(summary = "질문 작성", description = "일반 사용자가 질문 글을 작성한다.")
 	@ApiResponse(responseCode = "200", description = "성공")
 	public CommonResponse<QuestionResponse.Question> createQuestion(
 			@Schema(description = "질문글 생성 요청", implementation = QuestionResponse.Question.class)
 			QuestionResponse.Question request
 	);
 
-	@Operation(summary = "AI 채팅 세션 생성", description = "채팅 세션을 생성한다.")
+	@Operation(summary = "질문글 삭제", description = "특정 ID의 질문글을 삭제한다.")
 	@ApiResponse(responseCode = "200", description = "성공")
 	public CommonResponse<QuestionResponse.Question> deleteQuestion(
 			@Schema(description = "질문글 ID", example = "1")
 			Long id
 	);
 
-	@Operation(summary = "AI 채팅 세션 생성", description = "채팅 세션을 생성한다.")
+	@Operation(summary = "질문글 수정", description = "특정 ID의 질문글을 수정한다.")
 	@ApiResponse(responseCode = "200", description = "성공")
 	CommonResponse<QuestionResponse.Question> updateQuestion(
 			@Schema(description = "질문글 ID", example = "1")
