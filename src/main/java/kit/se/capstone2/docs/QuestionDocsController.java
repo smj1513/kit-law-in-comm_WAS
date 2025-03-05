07bf1ff9defb4e3dc6537a6fce243a7e9a540e03
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2025-03-05 19:32:18 +0900

    질문글 관련 API 설계 일부 추가

--- src/main/java/kit/se/capstone2/docs/QuestionDocsController.java
+++ src/main/java/kit/se/capstone2/docs/QuestionDocsController.java
@@ -11,7 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Tag(name = "QuestionDocsController", description = "질문글 관련 API 문서")
+@Tag(name = "질문글 관련 API", description = "질문글 관련 API 문서")
 public interface QuestionDocsController {
 	@Operation(summary = "질문글 목록 조회", description = "질문 글에 대한 목록을 페이지네이션으로 조회한다.")
 	@ApiResponse(responseCode = "200", description = "성공")
