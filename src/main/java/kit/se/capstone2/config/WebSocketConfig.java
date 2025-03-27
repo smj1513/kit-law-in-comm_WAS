d81ad0fdb30ceeca01bcc6a3e1b93472d6052cdb
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2025-03-28 02:10:09 +0900

    시큐리티 설정 변경

--- src/main/java/kit/se/capstone2/config/WebSocketConfig.java
+++ src/main/java/kit/se/capstone2/config/WebSocketConfig.java
@@ -7,21 +7,21 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@EnableWebSocket
-@EnableWebSocketMessageBroker
-@Configuration
+//@EnableWebSocket
+//@EnableWebSocketMessageBroker
+//@Configuration
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws")
-				.setAllowedOriginPatterns("*")
-				.withSockJS();
-	}
-
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.enableSimpleBroker("/queue");
-		registry.setApplicationDestinationPrefixes("/app");
-	}
+//
+//	@Override
+//	public void registerStompEndpoints(StompEndpointRegistry registry) {
+//		registry.addEndpoint("/ws")
+//				.setAllowedOriginPatterns("*")
+//				.withSockJS();
+//	}
+//
+//	@Override
+//	public void configureMessageBroker(MessageBrokerRegistry registry) {
+//		registry.enableSimpleBroker("/queue");
+//		registry.setApplicationDestinationPrefixes("/app");
+//	}
 }
