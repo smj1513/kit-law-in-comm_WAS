fd34112ca6b2e66ae15413a672fd31f096e49702
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2025-03-27 17:44:13 +0900

    swagger 설정 변경

--- src/main/java/kit/se/capstone2/config/SwaggerConfig.java
+++ src/main/java/kit/se/capstone2/config/SwaggerConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(servers = {
-		@Server(url = "http://http://202.31.202.38/api", description = "Remote Server URL"),
+		@Server(url = "http://202.31.202.38/api", description = "Remote Server URL"),
 		@Server(url = "http://localhost:8080/api", description = "Local Server URL")
 })
 @Configuration
