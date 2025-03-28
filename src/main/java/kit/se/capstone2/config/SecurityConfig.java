79bc477e10c87d230be23758dd3fb7d296cca472
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2025-03-28 20:08:12 +0900

    Jwt 설정 변경

--- src/main/java/kit/se/capstone2/config/SecurityConfig.java
+++ src/main/java/kit/se/capstone2/config/SecurityConfig.java
@@ -110,11 +110,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() { //cors 정책 설정 실 운영 들어가기전에 변경해야됨
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowedOriginPatterns(List.of(
-				"http://localhost:3000",
-				"http://202.31.202.38:*",
-				"http://202.31.202.38/api/*"  // Swagger UI 도메인 포함
-		));
+		corsConfiguration.setAllowedOriginPatterns(List.of("*"));
 		corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 		corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 		corsConfiguration.setExposedHeaders(List.of(
