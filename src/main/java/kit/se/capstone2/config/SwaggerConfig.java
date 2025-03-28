ddf0d63ecc6ea0d762022d3c9a3fad7987de6323
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2025-03-28 18:57:26 +0900

    swagger 설정 변경

--- src/main/java/kit/se/capstone2/config/SwaggerConfig.java
+++ src/main/java/kit/se/capstone2/config/SwaggerConfig.java
@@ -21,16 +21,15 @@ public class SwaggerConfig {
 	@Bean
 	public OpenAPI openAPI() {
 
-		String securityJwtName = "JWT";
-		SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+		String jwtSchemeName = "Authorization";
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList("Authorization");
 		Components components = new Components()
 				.addSecuritySchemes(
-						securityJwtName, new SecurityScheme()
+						jwtSchemeName, new SecurityScheme()
+								.name(jwtSchemeName)  // 헤더 이름 명시
 								.type(SecurityScheme.Type.HTTP)
 								.scheme("bearer")  // 소문자로 변경
-								.bearerFormat("JWT")
-								.name("Authorization")  // 헤더 이름 명시
-				);
+								.bearerFormat("JWT"));
 		Info info = new Info()
 				.version("v1.0")
 				.title("LC Inside API")
@@ -38,15 +37,15 @@ public class SwaggerConfig {
 								레전드 캡스톤 디자인2 프로젝트
 								AI 어시스턴트 기반 법률 상담 커뮤니티 플랫폼
 								LC Inside API 문서
-								
+						
 								---- 인증 관련 ----
 								1. 아래쪽 login-endpoint를 통해 로그인을 하고 JWT 토큰을 받아서 오른쪽 Authorize 버튼을 눌러서 JWT 토큰을 입력 후 사용.
-								
+						
 								---- 파일 처리 관련 ----
 								1. FileResponse로 오는 경로는 /2025/3/25/b1c2d6192ab14fcaa284a0d5d34638de.jpg 이와 같은 리소스 형식임
 								2. 프론트엔드에서 image 태그의 src 방식으로 사진을 조회하기 위해서는 버킷 경로 뒤에 붙여서 사용해야 함
 						""");
-		return new OpenAPI().info(info).addSecurityItem(securityRequirement).components(components);
+		return new OpenAPI().components(components).info(info).addSecurityItem(securityRequirement);
 	}
 
 
