07081abc59290bd00bfd57b5e57b71be7e6cc4ba
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2025-04-03 17:56:31 +0900

    채팅 관련 추가 작업

--- src/main/java/kit/se/capstone2/config/WebSocketConfig.java
+++ src/main/java/kit/se/capstone2/config/WebSocketConfig.java
@@ -1,11 +1,11 @@
 package kit.se.capstone2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
 
 @EnableWebSocket
 @EnableWebSocketMessageBroker // 송신자 -> (메시지 브로커) -> 수신자1, 수신자2 ...
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		// stomp 접속 주소 url = ws://{host}:8080/ws
 		registry.addEndpoint("/ws-stomp")
 				.setAllowedOriginPatterns("*");
-	//			.withSockJS();
+				//.withSockJS();
 	}
 
 
@@ -26,5 +26,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.enableSimpleBroker("/sub"); //스프링이 제공하는 인메모리 메시지 브로커 사용
 		registry.setApplicationDestinationPrefixes("/pub"); //메시지를 퍼블리싱하는 프리픽스 지정
+	}
+
+	@Override
+	public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+		registration.addDecoratorFactory(new WebSocketHandlerDecoratorFactory() {
+			@Override
+			public WebSocketHandler decorate(@Autowired WebSocketHandler webSocketHandler) {
+				return new CustomWebSocketHandlerDecorator(webSocketHandler);
+			}
+		});
 	}
 }
